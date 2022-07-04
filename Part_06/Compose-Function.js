a44ed5bd4bb7fd67a48f80abfc4024a4880f7b76
Mohosin Hasan Akash <107780771+Mohosin999@users.noreply.github.com>
2022-07-04 21:56:43 +0600

    Add files via upload

---
+++ Part_06/Compose-Function.js
@@ -0,0 +1,31 @@
+/**
+ * Compose Function
+ */
+
+// Understand Compose
+const add10 = (n) => n + 10;
+const times3 = (n) => n * 3;
+const div2 = (n) => n / 2;
+
+const n = 5;
+const x = div2(times3(add10(n)));
+console.log(x);
+
+// Compose Function - Long Way
+const compose = (...fns) => {
+  return (x) => {
+    return fns.reduce((v, f) => f(v), x);
+  };
+};
+
+const r1 = compose(add10, times3, div2)(n);
+console.log(r1);
+
+// Pipe Function - Shortest Way
+const pipe =
+  (...fns) =>
+  (x) =>
+    fns.reduce((v, f) => f(v), x);
+
+const r2 = pipe(add10, times3, div2)(n);
+console.log(r2);
